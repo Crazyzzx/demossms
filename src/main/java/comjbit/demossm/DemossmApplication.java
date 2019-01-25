58ceea20c7d7c8614ec1141874fbfb6f328b3a30
lenovo <1067037732@qq.com>
2019-01-25 15:54:08 +0800

    init commit

---
+++ src/main/java/comjbit/demossm/DemossmApplication.java
@@ -0,0 +1,14 @@
+package comjbit.demossm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemossmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemossmApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/comjbit/demossm/DemossmApplicationTests.java
@@ -0,0 +1,17 @@
+package comjbit.demossm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemossmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
